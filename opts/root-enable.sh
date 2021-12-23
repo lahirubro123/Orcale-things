0d3dadf3b7a90b5c045a99acb16f7955bcda7965
chnthkksn <47731399+chnthkksn@users.noreply.github.com>
2021-12-23 20:25:27 +0530

    Update root-enable.sh

--- opts/root-enable.sh
+++ opts/root-enable.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 echo""
-echo"Locating crontab directory"
+echo Locating crontab directory
 cd /etc/ssh/
 echo""
 
 echo""
-echo"Deleting default corntab config"
+echo Deleting default corntab config
 rm sshd_config
 echo""
 
 echo ""
-echo"Downloding modified crontab config"
+echo Downloding modified crontab config
 echo ""
 wget https://raw.githubusercontent.com/chnthkksn/Orcale-things/main/opts/sshd_config
 
 
 echo ""
-echo "Restarting sshd service"
+echo Restarting sshd service
 service sshd restart
 echo ""
