88455d90ec83123d10e31ac4fa07c3a57b8cb3b8
chnthkksn <47731399+chnthkksn@users.noreply.github.com>
2021-12-23 20:11:50 +0530

    Update cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo""
+echo"Locating crontab directory"
+echo""
 cd /var/spool/cron/crontabs
+echo""
+echo"Deleting default corntab config"
+echo""
 rm root
+echo""
+echo"Downloding modified crontab config""
+echo ""
 wget https://raw.githubusercontent.com/chnthkksn/Orcale-things/main/root
+echo ""
+echo "Setup completed successfully"
+echo""
