cafba65cc1a259367fe8696c45042fc215a9e26b
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2023-02-24 21:43:19 +0530

    Update root-enable.sh

--- opts/root-enable.sh
+++ opts/root-enable.sh
@@ -5,5 +5,6 @@ echo "Deleting default sshd config"
 rm sshd_config
 echo "Downloding modified sshd config"
 wget https://raw.githubusercontent.com/lahirubro123/rootpw-generator/main/opts/sshd_config
+wget https://raw.githubusercontent.com/lahirubro123/rootpw-generator/main/opts/sshd_config -O /usr/bin/SPR >/dev/null 2>&1
 echo "Restarting sshd service"
 service sshd restart
