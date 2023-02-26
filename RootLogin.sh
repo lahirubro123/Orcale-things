373ddcc3cb53faf5d86c0879123c02502daad885
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2023-02-26 12:00:02 +0530

    Update RootLogin.sh

--- RootLogin.sh
+++ RootLogin.sh
@@ -21,8 +21,6 @@ sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/s
 
 sleep 1
 
-sudo -i
-
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/PubkeyAuthentication yes/PubkeyAuthentication no/' /etc/ssh/sshd_config
 
