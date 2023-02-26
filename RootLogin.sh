fd1370ad51ab20f4dcbcf732dd13dbacc2e8ef8c
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2023-02-26 11:26:10 +0530

    Update RootLogin.sh

--- RootLogin.sh
+++ RootLogin.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 
+
+sudo passwd root
 apt-get update && apt-get upgrade
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication prohibit-password/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 sudo -i
 
-sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/PubkeyAuthentication yes/PubkeyAuthentication no/' /etc/ssh/sshd_config
 
 sudo service ssh restart
 sudo service sshd restart
-sudo passwd root
+
