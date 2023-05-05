9610ae3782c20fde91d2d91e2b7b2dfebd0a6c75
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2023-05-05 10:07:17 +0530

    Update ocfix.sh

--- ocfix.sh
+++ ocfix.sh
@@ -3,5 +3,6 @@ sudo wget -P /etc/sshfix https://raw.githubusercontent.com/lahirubro123/rootpw-g
 sudo chmod +x /etc/sshfix/sshexpirefix.sh
 sudo crontab -l > tempfile
 echo "@reboot /etc/sshfix/sshexpirefix.sh" >> tempfile
+echo "@reboot iptables -F" >> tempfile
 sudo crontab tempfile
 rm tempfile
